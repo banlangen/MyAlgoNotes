32f0fcbc18582b233d9f209110861b490d31bec0
Nianhui <wnh_008@163.com>
2018-01-09 00:37:16 +0800

    commit

--- leetcode/HTML/zoom_pageinfo.js
+++
@@ -1,8 +0,0 @@
-pageinfo = [[0,0,0,0,null],
-	[0,0,0,0,null],
-	[0,0,0,0,null],
-	[0,0,0,0,null]];
-pagedata = [ ["./3_-longest-substring-without-r.html","Introduction &#62; 3. Longest Substring Without Repeating Characters","Longest Sutstring Without Repeating Cnaracters  Brute Force 最简单最暴力的解法就是用两重循环，外层循环逐个遍历字符串数组的每个元素，然后再用内层循环向后探索寻找无重复字符的子串,这种做法的时间复杂度为O(n2),此处使用容...",""],
-["./5_-longest-palindromic-substri.html","Introduction &#62; 5. Longest Palindromic Substring","Longest Palindromic Substring  Manacher算法 最自然的解法就是以一个字符为中点依次左右展开对比，但是这样的做法面临奇数偶数的问题。也就是回文串存在以下的两种形式,在这种情况下面临着使用两套算法来判断的向题,极大的增加了解答问题的难度那么此处使用...",""],
-["./introduction.html","Introduction","Enter topic text here.",""],
-["./second-topic.html","Introduction &#62; 4. Median of Two Sorted Arrays","Mndian of two sorted arrays   本题的难度在于需要在logn的时需复杂度内完成.而能在log内的时间复杂度内完成的解法一般就是二分法。 可以使推主定就推导得出.由如下公式可以看出,在O(1)的时间内将问题的规模减少了一半,在这种情况下,l间复杂度就可以达...",""]];
